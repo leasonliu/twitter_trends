5629c53533c53381afbb8866ff7d8021f075be88
eason <yul196@pitt.edu>
2018-12-03 12:02:57 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,167 @@
+import { Component } from "@angular/core";
+
+import { setTheme } from "ngx-bootstrap/utils";
+import { BsDatepickerConfig } from "ngx-bootstrap/datepicker/public_api";
+import { HttpClient } from "@angular/common/http";
+import * as moment from "moment";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  bsValue = new Date();
+  bsRangeValue: Date[];
+  maxDate = new Date();
+  bsConfig: Partial<BsDatepickerConfig>;
+  colorTheme = "theme-blue";
+
+  constructor(private httpClient: HttpClient) {
+    setTheme("bs4"); // or 'bs3'
+    this.bsConfig = Object.assign({}, { containerClass: this.colorTheme });
+    this.bsValue.setDate(this.bsValue.getDate() - 7);
+    this.bsRangeValue = [this.bsValue, this.maxDate];
+  }
+
+  url = "http://localhost:9200/twitter_trend/_search";
+  queryBody = {
+    size: 0,
+    query: {
+      bool: {
+        must: {
+          match: {
+            woeid: 2473224
+          }
+        },
+        filter: {
+          range: {
+            time_stamp: {
+              gt: 1541030400,
+              lt: 1543629408
+            }
+          }
+        }
+      }
+    },
+    aggs: {
+      most_popular_trend: {
+        terms: {
+          field: "trend",
+          size: 100
+        },
+        aggs: {
+          max_popular: {
+            max: {
+              field: "popularity"
+            }
+          },
+          popular_trend_sort: {
+            bucket_sort: {
+              sort: [
+                {
+                  max_popular: { order: "desc" }
+                }
+              ],
+              size: 20
+            }
+          }
+        }
+      }
+    }
+  };
+  locations = [
+    {
+      id: 2473224,
+      name: "Pittsburgh"
+    },
+    {
+      id: 23424977,
+      name: "U.S"
+    },
+    {
+      id: 23424775,
+      name: "Canada"
+    },
+    {
+      id: 23424848,
+      name: "India"
+    },
+    {
+      id: 23424856,
+      name: "Japan"
+    },
+    {
+      id: 23424975,
+      name: "England"
+    },
+    {
+      id: 23424829,
+      name: "Germany"
+    },
+    {
+      id: 23424748,
+      name: "Australia"
+    }
+  ];
+  form = {
+    locationID: 0,
+    timeRange: null
+  };
+  search = "";
+  tweets = [];
+
+  async onSubmit() {
+    this.form.timeRange = this.bsRangeValue;
+    // id
+    this.queryBody.query.bool.must.match.woeid = this.form.locationID;
+    // time range
+    this.queryBody.query.bool.filter.range.time_stamp.gt = this.transformDateToUnix(this.form.timeRange[0]);
+    this.queryBody.query.bool.filter.range.time_stamp.lt = this.transformDateToUnix(this.form.timeRange[1]);
+
+    console.log(this.queryBody);
+    await this.sendPostRequest(this.queryBody).subscribe(
+      (data) => {
+        console.log(data);
+        // this.tweets = data;
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
+  async onSearch() {
+    console.log(this.search);
+    console.log(moment().unix());
+    // id
+    this.queryBody.query.bool.must.match.woeid = this.form.locationID;
+    // time range
+    this.queryBody.query.bool.filter.range.time_stamp.gt =
+      this.form.timeRange != null
+        ? this.transformDateToUnix(this.form.timeRange[0])
+        : 0;
+    this.queryBody.query.bool.filter.range.time_stamp.lt =
+      this.form.timeRange != null
+        ? this.transformDateToUnix(this.form.timeRange[1])
+        : moment().unix();
+
+    console.log(this.queryBody);
+    await this.sendPostRequest(this.queryBody).subscribe(
+      (data) => {
+        console.log(data);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
+  sendPostRequest(body) {
+    return this.httpClient.post(this.url, body, { observe: "response" });
+  }
+
+  transformDateToUnix(date: Date): number {
+    return moment(date, "X").unix();
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatSelectModule, MatFormFieldModule } from "@angular/material";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    BsDatepickerModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule { }
